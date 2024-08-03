1638c3b628aded27aac2789816aee13c235a1d8a
RudzaniNdaa <rudzani902@gmail.com>
2024-08-03 21:44:18 +0200

    LoggingBroker

---
+++ Blogs.API/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogs.API.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogTrace(string message);
+        void LogDebug(string message);
+        void LogWarning(string message);
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
