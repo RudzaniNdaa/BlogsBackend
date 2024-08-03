6a287a7bea0b35c20f56001f6e56c8f753d07b2b
RudzaniNdaa <rudzani902@gmail.com>
2024-08-03 21:40:36 +0200

    Inherit IDateTimeBroker

--- Blogs.API/Brokers/DateTimes/DateTimeBroker.cs
+++ Blogs.API/Brokers/DateTimes/DateTimeBroker.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Blogs.API.Brokers.DateTimes
 {
-    public class DateTimeBroker
+    public class DateTimeBroker : IDateTimeBroker
     {
         public DateTimeOffset GetCurrentDateTime() => DateTimeOffset.UtcNow;
     }
