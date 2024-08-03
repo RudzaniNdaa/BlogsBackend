8e7bb28d1632bce6b31b3d72d3dfa9138797c90e
RudzaniNdaa <rudzani902@gmail.com>
2024-08-04 00:07:13 +0200

    Reply Model

--- Blogs.API/Models/Replies/Reply.cs
+++ Blogs.API/Models/Replies/Reply.cs
@@ -2,19 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Blogs.API.Models.Replies;
 
-namespace Blogs.API.Models.Comments
+namespace Blogs.API.Models.Replies
 {
-    public class Comment
+    public class Reply
     {
         public Guid ID { get; set; }
-        public Guid AuthorID { get; set; }
-        public Guid PostID { get; set; }
+        public Guid CommentID { get; set; }
         public string Content { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
-
-        public IEnumerable<Reply> Replies { get; set; }
     }
 }
