73950e4ddf1a3755e16733203e831e348b6fb6c3
RudzaniNdaa <rudzani902@gmail.com>
2024-08-05 21:10:48 +0200

    Setting broker services under Program

--- Blogs.API/Program.cs
+++ Blogs.API/Program.cs
@@ -1,9 +1,28 @@
+using Blogs.API.Brokers.DateTimes;
+using Blogs.API.Brokers.Loggings;
+using Blogs.API.Brokers.Storages;
+using Blogs.API.Brokers.UserManagements;
+using Blogs.API.Models.Users;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StorageBroker>();
+
+builder.Services.AddIdentityCore<User>()
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<StorageBroker>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserManagementBroker, UserManagementBroker>();
+builder.Services.AddScoped<IStorageBroker, StorageBroker>();
+builder.Services.AddTransient<ILoggingBroker, LoggingBroker>();
+builder.Services.AddTransient<IDateTimeBroker, DateTimeBroker>();
 
 var app = builder.Build();
 
