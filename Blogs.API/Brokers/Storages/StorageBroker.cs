d5bb0ba96d6b2bb9d99454e99db996ab6f63ce72
RudzaniNdaa <rudzani902@gmail.com>
2024-08-05 11:51:21 +0200

    StorageBroker Setup

--- Blogs.API/Brokers/Storages/StorageBroker.cs
+++ Blogs.API/Brokers/Storages/StorageBroker.cs
@@ -2,11 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blogs.API.Models.Users;
+using EFxceptions.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blogs.API.Brokers.Storages
 {
-    public partial class StorageBroker
+    public partial class StorageBroker : EFxceptionsIdentityContext<User, Role, Guid>, IStorageBroker
     {
-        
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(
+                    name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
