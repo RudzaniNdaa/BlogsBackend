8a9afcf9107d87351878ca1c6186d1adc817d004
RudzaniNdaa <rudzani902@gmail.com>
2024-08-07 19:16:24 +0200

    Authors Controller

--- Blogs.API/Controllers/AuthorsController.cs
+++ Blogs.API/Controllers/AuthorsController.cs
@@ -20,13 +20,50 @@ namespace Blogs.API.Controllers
             this.authorProcessingService = authorProcessingService;
         }
 
+        [HttpPost]
+        public async ValueTask<ActionResult<Author>> PostAuthorAsync(Author author)
+        {
+            Author addAuthor =
+                await this.authorProcessingService.UpsertAuthorAsync(author);
+
+            return Ok(addAuthor);
+        }
+
         [HttpPut]
         public async ValueTask<ActionResult<Author>> PutAuthorAsync(Author author)
         {
             Author addAuthor =
                  await this.authorProcessingService.UpsertAuthorAsync(author);
 
-                return Ok(addAuthor);
+            return Ok(addAuthor);
         }
+
+        [HttpGet]
+        public ActionResult<IQueryable<Author>> GetAllAuthors()
+        {
+            IQueryable authors =
+                this.authorProcessingService.RetrieveAllAuthors();
+
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public async ValueTask<ActionResult<Author>> GetAuthorByIdAsync(Guid authorId)
+        {
+            Author maybeAuthor =
+                await this.authorProcessingService.RetrieveAuthorByIdAsync(authorId);
+
+            return Ok(maybeAuthor);
+        }
+
+        [HttpDelete("{authorId}")]
+        public async ValueTask<ActionResult<Author>> DeleteAuthorByIdAsync(Guid authorId)
+        {
+            Author deleteAuthor =
+                await this.authorProcessingService.TryRemoveAuthorByIdAsync(authorId);
+
+            return Ok(deleteAuthor);
+        }
+
     }
 }
